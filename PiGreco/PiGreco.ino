72e0044c37afe20c23f11b5484d5fad60861cf57
Luca <luca.bonora@barsanti.edu.it>
2019-05-11 09:22:19 +0200

    PrimoCodice

--- PiGreco/PiGreco.ino
+++ PiGreco/PiGreco.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+int bottoneI
+int bottone1
+int bottone2
+int bottone3
+int bottone4
+int bottone5
+int tempo = 1000;
+int vite = 3;
+LiquidCrystal lcd();
+
+void setup() {
+  pinMode(bottoneI,INPUT);
+  pinMode(bottone1,INPUT);
+  pinMode(bottone2,INPUT);
+  pinMode(bottone3,INPUT);
+  pinMode(bottone4,INPUT);
+  pinMode(bottone5,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(bottoneI == HIGH)
+  {
+    iniziaGioco();
+  }
 
+}
+
+void iniziaGioco()
+{
+  int pos = random(1,5);
+  String bottone = "bottone"+"pos"
+  lcd.setCursor(0,pos);
+  lcd.print("л");
+  for(int i=0,i<tempo,i++)
+  {
+    if(digitalRead(bottone) == HIGH)
+    {
+      vite++;
+      iniziaGioco();
+    }
+  }
+  vite--;
+  iniziaGioco();
+  
 }
