af2c3a9e6889da1449722339c0347e2fb31c1036
Luca <luca.bonora@barsanti.edu.it>
2019-05-24 13:27:55 +0200

    Code

--- PiGreco/PiGreco.ino
+++ PiGreco/PiGreco.ino
@@ -1,55 +1,71 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
 int bottoneI = 6;
-int bottone1 = 1;
-int bottone2 = 2;
-int bottone3 = 3;
-int bottone4 = 4;
-int bottone5 = 5;
 int tempo = 1000;
+int record = 0;
 int vite = 3;
 int punti = 0;
-LiquidCrystal lcd(13,12,11,10,9,8);
+LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
-  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
+  randomSeed(analogRead(0));
   pinMode(bottoneI,INPUT);
-  pinMode(bottone1,INPUT);
-  pinMode(bottone2,INPUT);
-  pinMode(bottone3,INPUT);
-  pinMode(bottone4,INPUT);
-  pinMode(bottone5,INPUT);
+  pinMode(1,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(4,INPUT);
+  pinMode(5,INPUT);
 }
 
 void loop() {
-  if(digitalRead(bottoneI == HIGH))
+  if(digitalRead(bottoneI) == HIGH)
   {
     
-    iniziaGioco();
+    iniziaGioco(vite, tempo, record, punti);
     
   }
   
 
 }
 
-void iniziaGioco()
+void iniziaGioco(int vite, int tempo, int record, int punti)
 {
-  lcd.setCursor(1,0);
-  lcd.print("vite: "+ vite);
+  
+  if (record<punti){
+    record = punti;
+  }
+  punti = 0;
+  if(vite == 0){
+    lcd.setCursor(0,0);
+    lcd.print("GAME OVER");
+    lcd.setCursor(0,1);
+    lcd.print("Record = "+String(record));
+  }
+  if(vite != 0){
+  lcd.setCursor(0,1);
+  lcd.print("Vite: "+ String(vite));
   while (vite>0){
-  int pos = random(1,5);
-  lcd.setCursor(0,pos);
-  lcd.print("л");
+  int pos = random(5);
+  lcd.setCursor(pos,0);
+  lcd.print("P");
   for(int i=0;i<tempo;i++)
   {
-    if(digitalRead(pos) == HIGH)
+    if(digitalRead(pos+1) == HIGH)
     {
       punti++;
-      iniziaGioco();
+      lcd.setCursor(pos,0);
+      lcd.print(" ");
+      iniziaGioco(vite, tempo, record, punti);
     }
+    delay(1);
+    
   }
   vite--;
-  iniziaGioco();
+  lcd.setCursor(pos,0);
+  lcd.print(" ");
+  iniziaGioco(vite, tempo, record , punti);
   }
-  
+  }
 }
